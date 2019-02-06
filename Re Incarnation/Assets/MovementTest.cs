533fb2ecbeece9635d5ebfe1661c61c0a0dd92d2
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-02-06 22:39:36 +0100

    A few things
    Moved the project because of buggyness.
    Added Player base model with animations. Idle, walk and roll
    Also added a testing script for movement.

---
+++ Re Incarnation/Assets/MovementTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour {
+
+CharacterController cc;
+[SerializeField] float speed = 10;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		Vector3 toMove = Vector3.zero;
+		toMove.x = Input.GetAxis("Horizontal");
+		toMove.z = Input.GetAxis("Vertical");
+		cc.Move(toMove * speed * Time.deltaTime);
+	}
+}
