2e95e7a873e8fadcbf6bbba4c588eeae1ab3958f
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2019-02-21 20:37:23 +0100

    Random stuff
    Flash

---
+++ Re Incarnation/Assets/Scripts/Hitbox.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Hitbox : MonoBehaviour
+{
+    [SerializeField] float curHealth = 3;
+    public int team = 0;
+    float maxHealth = 1;
+    public HitEvent hitEvent;
+    [SerializeField] float invincibleTime = 0f;
+    public UnityEvent deathEvent;
+
+    void Start()
+    {
+        StartStuff();
+    }
+
+    public void StartStuff()
+    {
+        maxHealth = curHealth;
+    }
+    public virtual void Hit(float damage)
+    {
+        if (IsInvoking("Invincible") == false)
+        {
+            curHealth -= damage;
+            HealthEvent(curHealth / maxHealth * 100);
+            hitEvent.Invoke(curHealth / maxHealth * 100);
+            if (curHealth <= 0)
+            {
+                Die();
+            }
+            Invoke("Invincible", invincibleTime);
+        }
+
+    }
+
+    void Invincible()
+    {
+        //invoke function
+    }
+
+    public void HealthEvent(float value)
+    {
+        hitEvent.AddListener(HealthEvent);
+    }
+
+
+
+    public virtual void Die()
+    {
+      //  Destroy(transform.root.gameObject);
+      deathEvent.Invoke();
+    }
+}
+
+[System.Serializable]
+public class HitEvent : UnityEvent<float> { }
---
+++ Re Incarnation/Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[RequireComponent(typeof(Rigidbody))]
+
+public class Hurtbox : MonoBehaviour
+{
+
+    [SerializeField] float damage = 1;
+    [SerializeField] int team = 0;
+    [SerializeField] bool destroyOnHit = true;
+    [SerializeField] UnityEvent hitEvent;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Hitbox hit = other.GetComponent<Hitbox>();
+        if (hit != null)
+        {
+            if (team != hit.team)
+            {
+                hit.Hit(damage);
+                hitEvent.Invoke();
+                if (destroyOnHit == true)
+                {
+                    Destroy(transform.root.gameObject);
+                }
+            }
+        }
+    }
+}
